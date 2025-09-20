1cb1db32ee5f1903aa27a8971d5874ab055e7230
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-09-19 22:07:16 -0300

    refactor: removes python/django files

--- contacts/urls.py
+++
@@ -1,8 +0,0 @@
-# Imports
-from django.urls import path
-from . import views
-
-# Urls
-urlpatterns = [
-	path('contact', views.contact, name='contact')
-]
--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-# Imports
-from django.urls import path
-from . import views
-
-# Pages urls
-urlpatterns = [
-	path('', views.index, name='index'),
-	path('about', views.about, name='about'),
-]
