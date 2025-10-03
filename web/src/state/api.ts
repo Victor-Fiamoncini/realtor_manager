e9915a5137a94bafc8f07892354ff6b27b228fec
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-10-03 19:48:09 -0300

    feat: implement getAuthUser endpoint to fetch authenticated user data and role

--- web/src/state/api.ts
+++ web/src/state/api.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { fetchAuthSession, getCurrentUser } from 'aws-amplify/auth'
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
@@ -6,7 +7,32 @@ export const api = createApi({
   }),
   reducerPath: 'api',
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getAuthUser: build.query<User, void>({
+      queryFn: async (_arg, _queryApi, _extraOptions, fetchWithBQ) => {
+        try {
+          const sessionResponse = await fetchAuthSession()
+          const { idToken } = sessionResponse.tokens || {}
+          const userRole = idToken?.payload['custom:role'] as string
+
+          const userResponse = await getCurrentUser()
+          const userDetailsResponse = await fetchWithBQ(
+            userRole === 'manager' ? `/managers/${userResponse.userId}` : `/tenants/${userResponse.userId}`
+          )
+
+          return {
+            data: {
+              cognitoInfo: { ...userResponse },
+              userInfo: userDetailsResponse.data as Tenant | Manager,
+              userRole: userRole as Tenant | Manager,
+            },
+          }
+        } catch {
+          return { error: 'Could not fetch user data' }
+        }
+      },
+    }),
+  }),
 })
 
 export const {} = api
