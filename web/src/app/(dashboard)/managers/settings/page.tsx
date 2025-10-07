c731eec89b97f7bdd82b9d1e7e7c09c493e10672
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-10-07 19:16:13 -0300

    feat: implement managers SettingsPage with form

--- web/src/app/(dashboard)/managers/settings/page.tsx
+++ web/src/app/(dashboard)/managers/settings/page.tsx
@@ -1,5 +1,34 @@
+'use client'
+
+import { useMemo } from 'react'
+
+import AppSettingsForm from '@/components/app-settings-form'
+import { useGetAuthUserQuery, useUpdateManagerSettingsMutation } from '@/state/api'
+
 const SettingsPage = () => {
-  return <div>SettingsPage</div>
+  const { data: user, isLoading: isAuthLoading } = useGetAuthUserQuery()
+
+  const [updateManager] = useUpdateManagerSettingsMutation()
+
+  const initialData = useMemo(() => {
+    if (!user?.userInfo) return { name: '', email: '', phoneNumber: '' }
+
+    return {
+      name: user.userInfo.name,
+      email: user.userInfo.email,
+      phoneNumber: user.userInfo.phoneNumber,
+    }
+  }, [user])
+
+  const handleSubmit = async (data: typeof initialData) => {
+    if (!user?.cognitoInfo) return
+
+    await updateManager({ cognitoId: user.cognitoInfo.userId, ...data })
+  }
+
+  if (isAuthLoading) return 'Loading...'
+
+  return <AppSettingsForm initialData={initialData} onSubmit={handleSubmit} userType="manager" />
 }
 
 export default SettingsPage
