28a31e88435c508234bfd2ad8a03e1bb43c98502
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-11-06 20:40:48 -0300

    refactor: remove loading state and simplify user authentication handling in LandingLayout

--- web/src/app/(nondashboard)/layout.tsx
+++ web/src/app/(nondashboard)/layout.tsx
@@ -1,16 +1,14 @@
 'use client'
 
 import { usePathname, useRouter } from 'next/navigation'
-import React, { PropsWithChildren, useEffect, useState } from 'react'
+import React, { PropsWithChildren, useEffect } from 'react'
 
 import AppNavbar from '@/components/app-navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
 import { useGetAuthUserQuery } from '@/state/api'
 
 const LandingLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  const [isRedirectLoading, setIsRedirectLoading] = useState(true)
-
-  const { data: user, isLoading: isAuthLoading } = useGetAuthUserQuery()
+  const { data: user } = useGetAuthUserQuery()
 
   const router = useRouter()
   const pathname = usePathname()
@@ -21,8 +19,6 @@ const LandingLayout: React.FC<PropsWithChildren> = ({ children }) => {
 
       if ((userRole === 'manager' && pathname.startsWith('/search')) || (userRole === 'manager' && pathname === '/')) {
         router.push('/managers/properties', { scroll: false })
-      } else {
-        setIsRedirectLoading(false)
       }
     }
   }, [user, router, pathname])
