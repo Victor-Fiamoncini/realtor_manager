5b735d5eda73023662c5d1d2edbadf885ce7bb0d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2025-11-19 18:15:45 -0300

    refactor: simplify LandingLayout by removing unused auth logic

--- web/src/app/(nondashboard)/layout.js
+++ web/src/app/(nondashboard)/layout.js
@@ -1,37 +1,18 @@
 'use client'
 
-import { usePathname, useRouter } from 'next/navigation'
-import React, { PropsWithChildren, useEffect } from 'react'
+import React, { useEffect } from 'react'
 
 import AppNavbar from '@/components/app-navbar'
 import { NAVBAR_HEIGHT } from '@/lib/constants'
-import { useGetAuthUserQuery } from '@/state/api'
 
-const LandingLayout = ({ children }) => {
-  const { data: user } = useGetAuthUserQuery()
+const LandingLayout = ({ children }) => (
+  <div className="h-full">
+    <AppNavbar />
 
-  const router = useRouter()
-  const pathname = usePathname()
-
-  useEffect(() => {
-    if (user) {
-      const userRole = user.userRole?.toLowerCase()
-
-      if ((userRole === 'manager' && pathname.startsWith('/search')) || (userRole === 'manager' && pathname === '/')) {
-        router.push('/managers/properties', { scroll: false })
-      }
-    }
-  }, [user, router, pathname])
-
-  return (
-    <div className="h-full">
-      <AppNavbar />
-
-      <main className="flex h-full w-full flex-col" style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
-        {children}
-      </main>
-    </div>
-  )
-}
+    <main className="flex h-full w-full flex-col" style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
+      {children}
+    </main>
+  </div>
+)
 
 export default LandingLayout
